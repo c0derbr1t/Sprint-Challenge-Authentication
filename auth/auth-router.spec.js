ad5958a03ad8e889b3fea0e425e662584daea5a6
c0derbr1t <c0der.br1t@gmail.com>
2020-02-28 12:42:15 -0600

    Created tests in jokes-router.spec.js

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const request = require("supertest");
 const server = require("../api/server.js");
 const db = require('../database/dbConfig.js');
-const Users = require('./auth-model.js');
 
 describe('router', function() {
     describe('environment', function() {
@@ -11,22 +10,13 @@ describe('router', function() {
         });
     });
 
-    // describe('clear tables', function() {
-    //     beforeEach(async function() {
-    //         await db('users').truncate();
-    //     });
-    // });
-
     describe('POST /api/register', function() {
         beforeEach(async function() {
             await db('users').truncate();
         });
-        // afterEach(async function() {
-        //     await db('users').truncate();
-        // });
 
         it('should add the new user', function() {
-            const payload = { username: "Elmo", password: "giggles" }
+            const payload = { username: "Elmo", password: "giggles" };
             return request(server).post('/api/auth/register').send(payload).then(res => {
                 expect(res.body.user.username).toBe('Elmo');
             });
@@ -36,6 +26,22 @@ describe('router', function() {
             const payload = { username: "Elmo", password: "giggles" };
             return request(server).post('/api/auth/register').send(payload).then(res => {
                 expect(res.status).toBe(201);
+            });
+        });
+    });
+
+    describe('POST /api/login', function() {
+        it('should log in the user', function() {
+            const payload = { username: "Elmo", password: "giggles" };
+            return request(server).post('/api/auth/login').send(payload).then(res => {
+                expect(res.status).toBe(200);
+            });
+        });
+
+        it('should contain a token', function() {
+            const payload = { username: "Elmo", password: "giggles" };
+            return request(server).post('/api/auth/login').send(payload).then(res => {
+                expect(res.text).toContain("token");
             });
         });
     });
